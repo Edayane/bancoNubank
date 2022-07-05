7922d98e497fa9c4ca44a111a1d53174d7ff727f
Edayane <108369358+Edayane@users.noreply.github.com>
2022-07-04 21:56:45 -0300

    Update Cliente.java

--- src/Cliente.java
+++ src/Cliente.java
@@ -3,7 +3,7 @@ public class Cliente extends Pessoa{
 	
 	private String idCliente;
 
-	
+	//teste
 	public Cliente(String nome, String cpf, Endereco endereco, String idCliente) {
 		super(nome, cpf, endereco);
 		this.idCliente = idCliente;
